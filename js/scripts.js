f9f70a6531d1f6093c8f03ff58283a7956afd258
Max Scher <maxobaxo@gmail.com>
2017-05-30 21:32:52 -0700

    reformat entire page into grid structure with jumbotron and container

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,13 @@ $(document).ready(function() {
   $("button#night").click(function() {
     $("body").removeClass();
     $("body").addClass("night-mode");
+    $("div#night-mode-text").removeClass();
+    $("div#night-mode-text").addClass("night-mode-text");
   });
   $("button#day").click(function() {
     $("body").removeClass();
     $("body").addClass("day-mode");
+    $("div#night-mode-text").removeClass();
+    $("div#night-mode-text").addClass("day-mode-text");
   });
   });
